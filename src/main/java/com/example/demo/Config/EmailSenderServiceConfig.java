1afc51105e50fbe9e474269eeecfbaecd674ccd5
Npierre250 <ndagijimanapierre96@gmail.com>
2023-11-27 01:10:45 +0200

    second

--- src/main/java/com/example/demo/Config/EmailSenderServiceConfig.java
+++ src/main/java/com/example/demo/Config/EmailSenderServiceConfig.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Service
 public class EmailSenderServiceConfig {
@@ -48,44 +46,6 @@ public class EmailSenderServiceConfig {
         this.sendEmail(toemail, subject, message);
     }
 
-    public void eventForApprovalNotification(String toemail, String subject, String Tousername,
-            String eventTitle, String invitedChoir, String choir,
-            String invitedChoirLocation, String date) throws MessagingException {
-        SimpleDateFormat sFormat = new SimpleDateFormat("MMM-dd HH:mm)");
-        String today = sFormat.format(new Date());
-        String message = "<body>\r\n" + //
-                "  <h1>\r\n" + //
-                "    Seventh-Day-Adventist\r\n" + //
-                "  </h1>\r\n" + //
-                "  <div>\r\n" + //
-                "    Hi Dear,<b>" + Tousername + "</b>\r\n" + //
-                " Choir " + invitedChoir + " from " + invitedChoirLocation + " needs for your approval </div>\r\n" + //
-                "  <div>\r\n" + //
-                " \r\n" + //
-                "  </div>\r\n" + //
-                "  <div>\r\n" + //
-                "   to participate " + eventTitle + " that has been prepared by " + choir + " that will take place on "
-                + date + "\r\n" + //
-                "  </div>\r\n" + //
-                "  <div>\r\n" + //
-                "    email use : <b>" + toemail + "</b>\r\n" + //
-                " <nav>   Done at : <b>" + today + "</b> </nav>\r\n" + //
-                "  </div>\r\n" + //
-                "</body>";
-        this.sendEmail(toemail, subject, message);
-    }
-    // @EventListener(ApplicationReadyEvent.class)
-    // public void sendEmail() throws MessagingException
-    // {
-    // this.sendEmail("ndagijimanapeter777@gmail.com","Login
-    // Successfully","<!doctype html><html lang=\"en\"><head><title>Sign
-    // up</title><body><h1 style='background-color:grey'>Inyange</h1><div
-    // style='background-color:white,border:1px solid
-    // black,border-radius:15'><h4>Welcome Page</h4><p>Dear , your account has been
-    // created successfully</p>"+
-    // "<a href='http://localhost:8000'>Visit
-    // website</a></div></div></body></html>");
-    // System.out.println("Executed");
-    // }
+  
 
 }
